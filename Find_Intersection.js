b00bcd47a4158861abda13758cebeba7407ab5e4
Richard <richard.ok1002@gmail.com>
2021-05-17 23:31:30 -0700

    Testing Git on work Laptop

---
+++ Find_Intersection.js
@@ -0,0 +1,13 @@
+// ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+// 1, 4, 13
+// ["1, 3, 9, 10, 17, 18", "1, 4, 9, 18"]
+// 1, 9, 10
+
+function FindIntersection(strArr) {
+
+  return strArr;
+
+}
+
+console.log(FindIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]));
+console.log(FindIntersection(["1, 3, 9, 10, 17, 18", "1, 4, 9, 18"]));
